bf459ae858be69c8e8ff0c206174dbff597b4089
Alexander <pchp250g8@tutamail.com>
2026-01-21 19:31:56 +0300

    Добавьте файлы проекта.

---
+++ ProductOfElems/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductOfElems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Input a count of elements");
+            Int32.TryParse(Console.ReadLine(), out var nElems);
+            var iArray = new int[nElems];
+            var oRnd = new Random();
+            for (int i = 0; i < nElems; i++)
+            {
+                var nItem = oRnd.Next(1, 20);
+                iArray[i] = nItem;
+            }
+            var nProduct = 1;
+            for (int i = 0; i < nElems; i++)
+            {
+                nProduct *= iArray[i];
+                Console.Write(iArray[i] + " ");
+            }
+            Console.WriteLine($"\r\nThe Product of {nElems} elements of the array is:{nProduct}");
+            Console.Read();
+        }
+    }
+}
---
+++ ProductOfElems/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ProductOfElems")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProductOfElems")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("98a22ac1-dec2-4712-b427-1789b24dfaa1")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
